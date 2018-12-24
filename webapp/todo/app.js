9a2037f95c2bdc979840afd985cb9f7f888bfbb4
Yuta Matsumura <y-matsumura@alterbooth.com>
2018-12-24 15:49:30 +0900

    Complete tutorial

--- webapp/todo/app.js
+++ webapp/todo/app.js
@@ -1,41 +1,68 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const CosmosClient = require("@azure/cosmos").CosmosClient;
+const config = require("./config");
+const TaskList = require("./routes/tasklist");
+const TaskDao = require("./models/taskDao");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const express = require("express");
+const path = require("path");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 
-var app = express();
+const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//Todo App:
+const cosmosClient = new CosmosClient({
+  endpoint: config.host,
+  auth: {
+    masterKey: config.authKey
+  }
+});
+const taskDao = new TaskDao(cosmosClient, config.databaseId, config.containerId);
+const taskList = new TaskList(taskDao);
+taskDao
+  .init(err => {
+    console.error(err);
+  })
+  .catch(err => {
+    console.error(err);
+    console.error("Shutting down because there was an error settinig up the database.");
+    process.exit(1);
+  });
+
+app.get("/", (req, res, next) => taskList.showTasks(req, res).catch(next));
+app.post("/addtask", (req, res, next) => taskList.addTask(req, res).catch(next));
+app.post("/completetask", (req, res, next) => taskList.completeTask(req, res).catch(next));
+app.set("view engine", "jade");
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+  const err = new Error("Not Found");
+  err.status = 404;
+  next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
